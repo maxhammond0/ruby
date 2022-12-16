a7eb906af151b6c9cf60def3b82f570bd2ba9b2c
Max Hammond <max.hammond21@gmail.com>
2022-12-17 08:29:36 +1100

    create files

---
+++ test.rb
@@ -0,0 +1,11 @@
+# the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+# change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+fruits.each do |fruit|
+  puts "A fruit of type: #{fruit}"
+end
+
+for i in (1...5) do
+  puts i
+end
